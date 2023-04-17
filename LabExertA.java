ac0911b76d3ea89f64f403a10a5042b1258e9b4f
Klenn Ohjie Pahimna <121370627+sanguirIS@users.noreply.github.com>
2023-04-17 07:52:18 -0700

    Add files via upload

---
+++ LabExertA.java
@@ -0,0 +1,12 @@
+public class LabExertA {
+    public static void main(String[] args) {
+        int faveNumber = 7;
+        String faveCartChar = "Doraemon";
+        char mi = 'V';
+        char[] nickNameArray = {'N', 'i', 'c', 'a'};
+
+        System.out.println(faveNumber + " is my favorite number.");
+        System.out.println("I love " + faveCartChar);
+        System.out.println("My name is Veronica " + mi + ". Velasquez. You can call me " + String.valueOf(nickNameArray) + ".");
+    }
+}
